285ac12cb067d346ec5a557779b0bf0e37bf703b
Maxiiiiiime <maxime.vanherreweghen2003@gmail.com>
2024-12-03 15:11:28 +0100

    notif enfin bon

--- notification.js
+++ notification.js
@@ -1,28 +1,39 @@
-const NotifMy = () => {
-
-    let myNotif = null
-
+//affiche les notifs
+const notifyMe = () => {
+    //alert('notify')
+    let myNotification = null
+    //on définit un contenu de Notification par défaut
     const options = {
-        body: "Corpds du message",
-        icon: "icons/favicon.32x32.png",
-        url:"https://cepegra.be"
+        body: "Envoyé par Pierre",
+        icon: "icons/favicon-32x32.png",
+        vibrate: [200,100,200,100,200,100,200],
+        url: "https://www.lesoir.be"
     }
-    if ( Notification.permission === "granted") {
-        myNotif = new Notification('Hello !!', options)
-    }
-
-}
-
-if(Notification.permission !== 'granted') {
-    if(confirm('Accepter les notifications ?')){
-        Notification.requestPermission()
-        .then( permission => {
-           
-            console.log(permission)
-            if ( permission === 'granted'){
+    // si mon navigateur ne supporte pas les notifications
+    if( !("Notification" in window) ) {
+        alert('Pas de notification dans ce navigateur')
+        // si oui et qu'on a les droits
+    } else if (Notification.permission === "granted") {
+        console.log("Notification possible")
+        // on affiche une notif de test
+        myNotification = new Notification('Hi me !', options)
+        // si pas les droits, on les demande
+    } else {
+        Notification.requestPermission().then( permission => {
+            // si on les a
+            if (permission === "granted") {
                 console.log("Notification possible")
+                // notif de test
+                myNotification = new Notification('Oh, Thank You !', options)
             }
         })
-            
     }
 }
+// demande des droits  à l'ouverture
+if(Notification.permission !== 'granted') {
+    if(confirm('Recevoir notifications ?')) {
+        notifyMe()
+    }
+}
+//on essaie une notif de test à l'ouverture
+notifyMe()
