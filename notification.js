65c80e3a32c72a485488e05f6b7f3ac19718f211
Maxiiiiiime <maxime.vanherreweghen2003@gmail.com>
2024-12-03 14:53:43 +0100

    notif test

---
+++ notification.js
@@ -0,0 +1,13 @@
+if(Notification.permission !== 'granted') {
+    if(confirm('Accepter les notifications ?')){
+        Notification.requestPermission()
+        .then( permission => {
+           
+            console.log(permission)
+            if ( permission === 'granted'){
+                console.log("Notification possible")
+            }
+        })
+            
+    }
+}
