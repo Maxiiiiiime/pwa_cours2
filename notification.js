9748f334ddd21c0cfe48b1b3c6b11f44c1069360
Maxiiiiiime <maxime.vanherreweghen2003@gmail.com>
2024-12-03 14:59:03 +0100

    notif update

--- notification.js
+++ notification.js
@@ -1,3 +1,20 @@
+const NotifMy = () => {
+
+    let myNotif = null
+
+    const options = {
+        body: "Corpds du message",
+        icon: "icons/favicon.32x32.png",
+        url:"https://cepegra.be"
+    }
+    if ( Notification.permission === "granted") {
+        myNotif = new Notification('Hello !!', options)
+    }
+
+}
+
+
+
 if(Notification.permission !== 'granted') {
     if(confirm('Accepter les notifications ?')){
         Notification.requestPermission()
