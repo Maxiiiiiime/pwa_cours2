5cc741afa6574e8ce5ced274b31be3265ed32426
Maxiiiiiime <maxime.vanherreweghen2003@gmail.com>
2024-12-03 11:44:44 +0100

    add install-boutton

---
+++ install.js
@@ -0,0 +1,25 @@
+const insatllBtn =document.querySelector('#installButton')
+let deferredPrompt
+
+window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault()
+    deferredPrompt = event
+    insatllBtn.style.display ='block'
+})
+
+insatllBtn.addEventListener('click', async (event) => {
+    event.preventDefault()
+
+    if(deferredPrompt) {
+        deferredPrompt.prompt()
+        const choiceUser = await deferredPrompt.userChoice
+        if (choiceUser == 'accepted') {
+            insatllBtn.style.display = "none"
+        }
+        deferredPrompt = null
+    }
+})
+
+window.addEventListener('appinstalled', event =>{
+    insatllBtn.style.display ="none"
+})
